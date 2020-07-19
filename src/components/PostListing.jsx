4523d6ca70313ff91b2b0e5cd739653be7c54ef7
Chris Bailey <chris@ghostly.io>
2020-07-19 10:48:53 -0400

    simplify post listing

--- src/components/PostListing.jsx
+++ src/components/PostListing.jsx
@@ -15,6 +15,7 @@ const PostListing = ({ post }) => {
           </span>{' '}
           {post.frontmatter.date}
         </p>
+        <p> | </p>
         <p>
           <span role="img" aria-label="coffee">
             ☕️
@@ -22,47 +23,37 @@ const PostListing = ({ post }) => {
           {post.timeToRead}min/s to read
         </p>
       </InfoBar>
-      <Excerpt>
-        <p>{post.excerpt}</p>
-      </Excerpt>
     </ListingBox>
   )
 }
 
 const ListingBox = styled.article`
-  border-bottom: 4px solid var(--color-purple-light);
   padding: 2px;
-  margin-bottom: 2em;
+  margin-bottom: 1.5em;
 `
 
-const Title = styled.h1`
-  font-size: 26px;
+const Title = styled.h3`
   text-decoration: none;
   margin: 0;
   a {
     color: inherit;
     text-decoration: none;
+    padding: 4px;
     &:hover {
       text-decoration: none;
+      color: ${({ theme }) => theme.body};
+      background-color: ${({ theme }) => theme.heading};
     }
   }
 `
 
 const InfoBar = styled.div`
   display: flex;
-  justify-content: space-between;
   margin-bottom: 5px;
   p {
     font-size: 16px;
     margin: 0;
-  }
-`
-
-const Excerpt = styled.div`
-  margin: 0;
-  margin-bottom: 5px;
-  p {
-    margin: 0;
+    padding: 4px;
   }
 `
 
