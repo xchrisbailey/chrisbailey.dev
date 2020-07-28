e841725531000e33426019b713232a8c341b0ffd
Chris Bailey <chris@ghostly.io>
2020-07-27 22:30:36 -0400

    check for window in uselocalstorage

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -6,7 +6,9 @@ export default function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       // Get from local storage by key
-      const item = window.localStorage.getItem(key)
+      const item =
+        typeof window !== 'undefined' && window.localStorage.getItem(key)
+      // const item = window.localStorage.getItem(key)
       // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue
     } catch (error) {
