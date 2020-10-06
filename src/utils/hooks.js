6c632de10a31dbea1ec4ed78279d902235b0dea5
Chris Bailey <chris@ghostly.io>
2020-10-06 00:21:53 -0400

    chore: dep bump

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -2,18 +2,13 @@
 import { useState } from 'react'
 
 export default function useLocalStorage(key, initialValue) {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      // Get from local storage by key
       const item =
         typeof window !== 'undefined' && window.localStorage.getItem(key)
-      // const item = window.localStorage.getItem(key)
-      // Parse stored json or if none return initialValue
+
       return item ? JSON.parse(item) : initialValue
     } catch (error) {
-      // If error also return initialValue
       console.log(error)
       return initialValue
     }
