cb987d51600659d9fe65cf29c7fea96b9a0776db
Chris Bailey <chris@ghostly.io>
2021-01-02 10:04:48 -0500

    feat: disable crawling
    moving main site to different host, saving this for linking later

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,12 @@ module.exports = {
     lang: 'en',
   },
   plugins: [
-    `gatsby-plugin-robots-txt`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', disallow: ['/'] }],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-next-seo`,
@@ -97,9 +102,5 @@ module.exports = {
         icon: `src/images/rocket-icon.png`, // This path is relative to the root of the site.
       },
     },
-
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
